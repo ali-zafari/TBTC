3722b2924a91bca7707b639a86b69a8ff051883f
Ali Zafari <zafarimq@gmail.com>
2023-07-10 18:16:35 -0400

    [ENH] Update config file

--- lit_config.py
+++ lit_config.py
@@ -1,15 +1,15 @@
 class Config:
 
     class Model:
-        model = 'bmshj2018-factorized'  # choose one from compressai.zoo.image_models
-        quality = 1
+        model = 'zyc2022-swint-charm'  # choose one from compressai.zoo.image_models
+        quality = 'M'
         lmbda = 0.01
         beta = None
 
     class Data:
-        dataset_directory = '/mnt/abbb7b18-d9ab-4b62-98ca-c90e47f31d4d/CLICtrainCLICvalidKodaktest'
+        dataset_directory = 'path/to/dataset'
 
-        train_batch_size = 4
+        train_batch_size = 8
         valid_batch_size = 1
 
         train_patch_size = (256, 256)
@@ -23,17 +23,18 @@ class Config:
         accelerator = 'gpu'
         devices = [1, ]  # list of devices to train on
 
-        num_epochs = 2
+        num_epochs = 5000
         seed = 10
         net_lr = 0.0001
         aux_lr = 0.0001
 
-        validation_cadence = 1  # [epochs]
+        validation_cadence = 10  # [epochs]
         gradient_clip_norm = 10.0  # necessary for gradient-exploding-free training
 
         checkpoint_to_resume = None  # 'path/to/a/checkpoint'
         log_cadence = 25  # [steps] Note: global_steps=accumulated total number of calling step() for any optimizer
                           # but this value gets compared to the number of training_steps which is counted only once for
                           # each usage of training_step
+        
         strategy = 'auto'  # "ddp_find_unused_parameters_true" or "auto"
         float32_matmul_precision = None
